c3ce10c2667c8640984449eddc9b323d7efc79d0
xgqfrms <xgqfrms@outlook.com>
2016-04-14 18:21:12 +0800

    device
    plugins-android

--- HTML5/hello/platforms/android/platform_www/cordova_plugins.js
+++ HTML5/hello/platforms/android/platform_www/cordova_plugins.js
@@ -4,12 +4,20 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.2.1"
+    "cordova-plugin-whitelist": "1.2.1",
+    "cordova-plugin-device": "1.1.1"
 };
 // BOTTOM OF METADATA
 });
